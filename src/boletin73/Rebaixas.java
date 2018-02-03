1445919e10acb6ad53f64e2cb65c18624dd3a476
Juan <Juan@DESKTOP-J5PU6RU>
2018-02-03 20:29:54 +0100

    Boletin 72 acabado.

---
+++ src/boletin73/Rebaixas.java
@@ -0,0 +1,15 @@
+
+package boletin73;
+
+public class Rebaixas {
+    
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+//3- Crea unha clase Rebaixas cun método calculaPorcentaxe . Este método recibirá o prezo original e o rebaixado do producto . 
+//calculará a porcentaxe e, retornará éste valor 
