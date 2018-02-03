ad4e89afa23ef64042aa1bcc3b0d54a6532ecc9b
Juan <Juan@DESKTOP-J5PU6RU>
2018-02-03 21:01:34 +0100

    Boletin 73 acabado.

--- src/boletin73/Rebaixas.java
+++ src/boletin73/Rebaixas.java
@@ -2,14 +2,21 @@
 package boletin73;
 
 public class Rebaixas {
+    public float calculaPorcentaxe(int prezo,int rebaixado){
+       float rebaixa= (prezo-rebaixado);
+       float porcentaxe= (rebaixa/prezo)*100;
+       return porcentaxe;
+    }
     
     
     
     public static void main(String[] args) {
-        
+        Rebaixas jdk=new Rebaixas();
+        System.out.println("A rebaixa é do "+jdk.calculaPorcentaxe(2431,1624)+"%");
     }
     
 }
 
-//3- Crea unha clase Rebaixas cun método calculaPorcentaxe . Este método recibirá o prezo original e o rebaixado do producto . 
+//3- Crea unha clase Rebaixas cun método calculaPorcentaxe . 
+//Este método recibirá o prezo original e o rebaixado do producto . 
 //calculará a porcentaxe e, retornará éste valor 
